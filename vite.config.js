db935afd9875c54b0a173bcbdd00103a11728905
subodhpun <subodhmagar07@gmail.com>
2024-07-26 10:16:48 +0545

    deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   },
 });
